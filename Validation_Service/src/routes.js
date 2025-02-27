a31da3022c94bbe49c2875d378f15de89841b48b
pdemicheli <pietro@demicheli.com>
2025-02-26 23:19:18 -0700

    Added TODOs for signature checks, added new proof of task in validator service

--- Validation_Service/src/routes.js
+++ Validation_Service/src/routes.js
@@ -7,11 +7,9 @@ const validatorService = require("./validator.service");
 const router = Router()
 
 router.post("/validate", async (req, res) => {
-    const proofOfTask = req.body.proofOfTask;
-    const data = req.body.data;
-    const taskDefinitionId = req.body.taskDefinitionId;
-
-    console.log(`Validate task: proof of task: ${proofOfTask}`);
+    const { proofOfTask, data, taskDefinitionId } = req.body;
+    
+    console.log(`Validating task with proof of task: ${proofOfTask}`);
 
     try {
         const result = await validatorService.validate(proofOfTask, data, taskDefinitionId);
